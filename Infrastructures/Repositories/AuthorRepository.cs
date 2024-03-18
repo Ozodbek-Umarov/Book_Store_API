95edab5551d8689352e6c3b2e94b400f6a27460b
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-03-18 09:43:40 +0500

    added AuthorRepository

---
+++ Infrastructures/Repositories/AuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Infrastructures.Inrerfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructures.Repositories;
+
+public class AuthorRepository(AppDbContext dbContext) : Repository<Author>(dbContext), IAuthorInterface
+{
+
+    public async Task<List<Author>> GetAllWithReleationsAsync()
+        => await _dbContext.Authors
+            .Include(a => a.Books)
+            .ToListAsync();
+}
+
