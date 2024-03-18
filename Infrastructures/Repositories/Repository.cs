1ad619081839df2b662847969b69e4282b19468c
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-03-18 09:22:57 +0500

    added repository

--- Infrastructures/Repositories/Repository.cs
+++ Infrastructures/Repositories/Repository.cs
@@ -1,8 +1,37 @@
-﻿using Infrastructures.Inrerfaces;
+﻿using Domain.Entities;
+using Infrastructures.Inrerfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructures.Repositories;
 
-public class Repository
+public class Repository<T>(AppDbContext dbContext)
+            : IRepository<T> where T
+            : BaseEntity
 {
-   
+    protected readonly AppDbContext _dbContext = dbContext;
+    private readonly DbSet<T> _dbSet = dbContext.Set<T>();
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<T>> GetAllAsync()
+        => await _dbSet.ToListAsync();
+
+    public async Task<T?> GetByIdAsync(int id)
+        => await _dbSet.FirstOrDefaultAsync(e => e.Id == id);
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _dbContext.SaveChangesAsync();
+    }
 }
